bfa4cc711d4b4d0b0841c72ab5fe858e31d3d0b7
amoxycillin <almodan@outlook.com>
2023-02-09 13:47:25 +0800

    *全新版本

--- changelog.md
+++ changelog.md
@@ -1,74 +1,13 @@
-# 22.12.20
+# 23.2.9
 
 # 重要修改
 
-- 兼容安卓13的MIUI14(@dreamflandre)
-- QuickEdit高级版
-- 动漫之家社区版
+- 开启全新版本
+- 同步官方配置
+- 目前兼容miui14/13
 
 # 其他说明
 
-- 本次更新由@dreamflandre提供
+- 脚本重构，解决刷入报错问题
+- excel表格有过时嫌疑，目前暂时移除
 - 再次欢迎各位大佬多多提交PR
-
-
-# 22.9.10
-
-# 重要修改
-
-- 华为工作？（com.huawei.works）
-- 秋风蚂蚁森林快速收取能量（pansong291.xposed.quickenergy）
-- 抖音极速版（com.ss.android.ugc.aweme.lite）
-- 合并 GITHUB@MidNightBlackCat PR
-
-# 其他说明
-
-- 本次已装载78个配置文件
-- 欢迎各位大佬多多提交PR
-- 祝各位中秋快乐，预祝国庆快乐
-
-
-# 22.08.29
-
-## 重要修改
-
-- 什么值得买（com.smzdm.client.android）
-- 抖音（com.ss.android.ugc.aweme）
-
-## 其他修改
-
-- 本次已装载76个配置文件
-- TIM（com.tencent.tim）活动混乱，建议使用QQ，适配周期延长
-- 尝试修正交管12123（com.tmri.app.main），测试无效
-
-
-# 22.08.15
-
-## 重要修改
-
-- 修正了脚本问题
-- 修复拼多多（com.xunmeng.pinduoduo）
-- 修正TIM（com.tencent.tim）效果
-- 支付宝（com.eg.android.AlipayGphone）
-
-
-## 其他修改
-
-- 已统计配置文件模板
-- 已知交管12123（com.tmri.app.main）失效
-
-
-# 22.08.14
-
-## 重要部分
-
-- 首次版本修改于K40 13.0.6.0
-- 淘宝（com.taobao.taobao）
-- 闲鱼（com.taobao.idlefish）
-- 阿里巴巴（com.alibaba.wireless）
-- TIM（com.tencent.tim）
-- 拼多多（com.xunmeng.pinduoduo）
-
-## 其他修改
-
-- 本次已装载74个配置文件
--- customize.sh
+++ customize.sh
@@ -1,50 +1,75 @@
-#默认不动
-SKIPMOUNT=false
-#如果想启用system.prop的参数，则改为true
-PROPFILE=false
-#开机前执行的命令，想启用则改为true
-POSTFSDATA=false
-#开机后执行的命令，想启用则改为true
-LATESTARTSERVICE=false
+SKIPUNZIP=0
 
-#安装模块时打印的信息，不需要的部分可以自己删除，也可以自己添加。
-print_modname() {
-ui_print "
- ****************************
- - 模块: $MODNAME
- - 作者: $MODAUTHOR
- ****************************
- - 设备代号: $device
- - 安卓版本: Android $Android
- - MIUI版本: $Version
- ****************************
- "
- sleep 0.05
- ui_print " * 仅适用于运行官方 MIUI 13/14 的设备"
- sleep 0.05
- ui_print " * 其他设备安装可能造成未知影响！"
- sleep 0.05
- ui_print " * Telegram Group: @darkp_miui "
+# 获取基础环境信息
+MODversion=`grep_prop version $TMPDIR/module.prop`
+MODdescription=`grep_prop description $TMPDIR/module.prop`
+device=`getprop ro.product.device`
+Version=`getprop ro.build.version.incremental`
+Android=`getprop ro.build.version.release`
+Sdk=`getprop ro.build.version.sdk`
+
+# 获取音量键状态
+get_choose()
+{
+	local choose
+	local branch
+	while :; do
+		choose="$(getevent -qlc 1 | awk '{ print $3 }')"
+		case "$choose" in
+		KEY_VOLUMEUP)  branch="0" ;;
+		KEY_VOLUMEDOWN)  branch="1" ;;
+		*)  continue ;;
+		esac
+		echo "$branch"
+		break
+	done
 }
 
-#开始安装（shell命令）
-on_install() {
-    ui_print " "
-    ui_print " - Extracting module files"
-    unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
-#检测文件存在
-    if [ -f "/system_ext/etc/forcedarkconfig/ForceDarkAppSettings.json" ] ; then
-		ui_print "- 检测为MIUI14更改配置文件位置以适配"
-        mkdir -p $MODPATH/system/system_ext/etc/forcedarkconfig/
-		mv -f $MODPATH/system/etc/forcedarkconfig/* $MODPATH/system/system_ext/etc/forcedarkconfig/
-        mv -f $MODPATH/system/etc/ForceDarkAppSettings.json $MODPATH/system/system_ext/etc/forcedarkconfig/ForceDarkAppSettings.json
-		rm -rf $MODPATH/system/etc
-    fi
-#install_module
-#abort 
-#用于停止/清除安装时需要的命令，正常情况下用不到，请删除后写入你的shell命令。
+#安装模块时打印的信息
+UiPrint() 
+{
+	echo "$@"
+	sleep 0.03
 }
-#设置权限
-set_permissions() {
-set_perm_recursive  $MODPATH  0  0  0755  0644
-}
+UiPrint "****************************"
+UiPrint "- 模块: $MODNAME"
+UiPrint "- 作者: $MODAUTH"
+UiPrint "****************************"
+UiPrint "- 设备代号: $device"
+UiPrint "- 安卓版本: Android $Android"
+UiPrint "- MIUI版本: $Version"
+UiPrint "****************************"
+UiPrint "* 仅适用于运行官方 MIUI 14 的设备 "
+UiPrint "* 其他设备/系统安装可能造成未知影响！"
+UiPrint "? 确定安装此模块吗？(请选择)" 
+UiPrint "- 按音量键+ : 安装"
+UiPrint "- 按音量键- : 取消"
+ 
+if [[ $(get_choose) == 0 ]]; then
+	UiPrint "- 已选择安装 $MODNAME"
+	UiPrint " "
+	if [[ -f "/system_ext/etc/forcedarkconfig/ForceDarkAppSettings.json" ]] ; then
+		UiPrint "- 环境检测版本: $Version"
+        UiPrint "- 检测到14配置，您是 MIUI14 系统"
+        UiPrint "? 是否确定安装，版本不对应可能造成无法开机?"
+		UiPrint "- 按音量键+ : 安装"
+        UiPrint "- 按音量键- : 取消"
+		if [[ $(get_choose) == 0 ]]; then
+			UiPrint "- 开始 MIUI14 配置安装模式"
+            unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+            mkdir -p $MODPATH/system/system_ext/etc/forcedarkconfig/
+	        mv -f $MODPATH/system/etc/forcedarkconfig/* $MODPATH/system/system_ext/etc/forcedarkconfig/
+            mv -f $MODPATH/system/etc/ForceDarkAppSettings.json $MODPATH/system/system_ext/etc/forcedarkconfig/ForceDarkAppSettings.json
+	        rm -rf $MODPATH/system/etc
+            UiPrint "* 模块安装完成"
+        else
+            abort "* 已经选择退出安装"
+		fi
+	else
+		UiPrint "- 开始 MIUI13 配置安装模式"
+        unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+		UiPrint "* 模块安装完成"
+	fi
+else
+    abort "* 已经选择退出安装"
+fi
